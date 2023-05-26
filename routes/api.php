172c32000116f8a7c1868a99dafae6e231990604
Renato Lucena <r.lucena@xsensors.ai>
2023-05-26 00:50:26 -0300

    estrutura docker sail + passport

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('info', function () {
+    $app_info = [
+        'server' => getenv('APP_NAME'),
+        'version' => getenv('APP_VERSION'),
+    ];
+
+    return response()->json($app_info);
 });
